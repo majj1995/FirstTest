b3abeb375ca8cf997916faae7cc1687c9a37835d
马佳君 <595641927@qq.com>
2018-10-22 19:08:07 +0800

    6714(2)

--- 6714_spec_stage2/6714_spec_stage2/todo.py
+++ 6714_spec_stage2/6714_spec_stage2/todo.py
@@ -1,33 +1,47 @@
 import torch
 from config import config
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import torch.nn.functional as F
 
 _config = config()
 
 
 def evaluate(golden_list, predict_list):
-    parm1 = len(golden_list)
-    parm2 = len(predict_list)
-    list1 = []
-    list2 = []
-    for item_1 in golden_list:
-        element = []
-        for index in range(len(item_1)):
-            if "TAG" in item_1[index]:
-                element.append({item_1[index]: index})
-            if "HYP" in item_1[index]:
-                element.append({item_1[index]: index})
-        list1.append(element)
-    for item_2 in predict_list:
-        element = []
-        for index in range(len(item_2)):
-            if "TAG" in item_2[index]:
-                element.append({item_2[index]: index})
-            if "HYP" in item_2[index]:
-                element.append({item_2[index]: index})
-        list2.append(element)
-    list3 = [x for x in list1 if x in list2]
-    match_num = len(list3)
-    return 2*match_num/(parm1 + parm2)
+    parm1 = 0
+    parm2 = 0
+    match_num = 0
+    F1 = 0
+    for idx_i in range(len(golden_list)):
+        element1 = golden_list[idx_i]
+        element2 = predict_list[idx_i]
+        for idx_j in range(len(element1)):
+            if element1[idx_j] == element2[idx_j] and "B" in element1[idx_j]:
+                index = idx_j + 1
+                flag = 1
+                while(index < len(element1)-1 and flag == 1):
+                    if element1[index] == element2[index] and element1[index] == "O":
+                        break
+                    elif element1[index] == element2[index] and "I" in element1[index]:
+                        index += 1
+                    elif element1[index] != element2[index]:
+                        flag = 0
+                        break
+                    elif element1[index] == element2[index] and "B" in element1[index]:
+                        break
+                if flag == 1:
+                    match_num += 1
+
+        for idx_j in range(len(element1)):
+            if "B" in element1[idx_j]:
+                parm1 += 1
+            if "B" in element2[idx_j]:
+                parm2 += 1
+
+    if parm1 + parm2 == 1:
+        F1 = 1
+    else:
+        F1 = 2 * match_num / (parm1 + parm2)
+    return F1
 
 
 def new_LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
@@ -53,27 +67,23 @@ def new_LSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None):
 
 
 def get_char_sequence(model, batch_char_index_matrices, batch_word_len_lists):
-    size_of_batch_char_index = batch_char_index_matrices.size()
-    word_max_length = size_of_batch_char_index[2]
-    batch_char_index_list = torch.reshape(batch_char_index_matrices, (-1, word_max_length))
-
     size_of_batch_word_len = batch_word_len_lists.size()
-    batch_word_len_list = torch.reshape(batch_word_len_lists, (-1,))
+    batch_char_index_list = batch_char_index_matrices.view((size_of_batch_word_len[0]*size_of_batch_word_len[1], -1))
+    batch_word_len = batch_word_len_lists.view(-1)
     char_embeddings = model.char_embeds(batch_char_index_list)
-    input_embeddings = model.non_recurrent_dropout(char_embeddings)
+    parm_index, sorted_batch_word_len_list = model.sort_input(batch_word_len)
+    sorterd_embeddings = char_embeddings[parm_index]
+    _, original_index = torch.sort(parm_index, descending=False)
 
-    parm_index, sorted_batch_world_len_list = model.sort_input(batch_word_len_list)
-    sorterd_embeddings = input_embeddings[parm_index]
-    _, original_index = model.sort_input(parm_index, descending=False)
-
-    result_sequence = pack_padded_sequence(sorterd_embeddings, lengths=sorted_batch_world_len_list.data.tolist(),
-                                           batch_first=True)
-    result_sequence, state = model.char_lstm(result_sequence)
+    result_sequence = pack_padded_sequence(sorterd_embeddings, lengths=sorted_batch_word_len_list.data.tolist(), batch_first=True)
+    result_sequence, _ = model.char_lstm(result_sequence)
     result_sequence, _ = pad_packed_sequence(result_sequence, batch_first=True)
-    result_sequence = torch.cat([result_sequence[:, -1, :model.config.char_lstm_output_dim],
-                                 result_sequence[:, 0, model.config.char_lstm_output_dim:]], -1)
+    sequence = []
+    for i in range(len(sorted_batch_word_len_list)):
+        sequence.append(torch.cat([result_sequence[i, sorted_batch_word_len_list[i] - 1, :model.config.char_lstm_output_dim], result_sequence[i, 0, model.config.char_lstm_output_dim:]],-1).unsqueeze(0))
+    result_sequence = torch.cat(sequence, 0)
     result_sequence = result_sequence[original_index]
-    result_sequence = model.non_recurrent_dropout(result_sequence)
-    result_sequence = torch.reshape(result_sequence, (size_of_batch_word_len[0], size_of_batch_word_len[1], -1))
+    result_sequence = result_sequence.view((size_of_batch_word_len[0], size_of_batch_word_len[1], -1))
     return result_sequence
+
 
